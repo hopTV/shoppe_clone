9866b8cc984129ee92ac6afbfd161fcc992a46a3
hop tran <hoptran270399@gmail.com>
2023-08-05 01:17:55 +0800

    update product detail

---
+++ src/components/quantityController/QuantityController.tsx
@@ -0,0 +1,48 @@
+import InputNumber from '../inputNumber'
+
+const QuantityController = () => {
+  return (
+    <div className='ml-6 flex items-center'>
+      <button
+        className='flex h-8 w-8 items-center justify-center rounded-l-md border border-gray-300
+      text-gray-600
+      '
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='h-4 w-4'
+        >
+          <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 12h-15' />
+        </svg>
+      </button>
+      <InputNumber
+        value={1}
+        className=''
+        classNameError='hidden'
+        classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
+      />
+      <button className='border0gray-300 flex h-8 w-8 items-center justify-center rounded-r-md border'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='h-4 w-4'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M12 4.5v15m7.5-7.5h-15'
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default QuantityController
